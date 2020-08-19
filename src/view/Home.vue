bf6a24294f9a0b4fddfc0ad61a76a565a9e6b46c
xzw <1272117264@qq.com>
2020-08-19 20:48:08 +0800

    搭建网页结构

---
+++ src/view/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>Gulu UI</h1>
+            <h2>a beautiful ui</h2>
+            <div class="actions">
+                <a href="">Github</a>
+                <a href="">开始</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>

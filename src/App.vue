4adc52d232d4063dd1e06f1f275a76a8a456f808
xzw <1272117264@qq.com>
2020-08-20 16:14:49 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
 </template>
 
 <script>
+import { ref, provide } from 'vue'
 
 export default {
   name: 'App',
+  setup(){
+    const menuvisible = ref(false)
+    provide('menuvisible', menuvisible)
+  }
+
 }
 </script>

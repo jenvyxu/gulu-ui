ac2d4d3ee39d165e96eb8621995db819082568a5
xzw <1272117264@qq.com>
2020-08-20 16:48:56 +0800

    路由切换关闭aside

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script>
 import { ref, provide } from 'vue'
+import { router } from './router'
 
 export default {
   name: 'App',
@@ -11,7 +12,11 @@ export default {
     const width = document.documentElement.clientWidth
     const menuvisible = ref(width <= 500 ? false : true)
     provide('menuvisible', menuvisible)
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuvisible.value = false
+      }
+    })
   }
-
 }
 </script>

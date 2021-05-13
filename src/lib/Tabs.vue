e30c67bd8ac37bc30ec56e3ebc8541b4e6798bc4
xzw <1272117264@qq.com>
2021-05-14 04:49:13 +0800

    feat: 设置指示条的宽度和位置

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="jen-tabs">
-    <div class="jen-tabs-nav">
+    <div class="jen-tabs-nav" ref="container">
       <div class="jen-tabs-nav-item"
         :class="{selected: t === selected}"
         v-for="(t, index) in titles"
         :key="index"
+        :ref="el => { if (selected === t) selectedItem = el }"
         @click="select(t)">
         {{t}}
         </div>
+        <div class="jen-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="jen-tabs-content">
       <component :is="current" :key="selected"/>
@@ -15,7 +17,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted, onUpdated, ref, watchEffect } from 'vue'
 import Tab from './Tab.vue'
 export default {
   props: {
@@ -41,11 +43,28 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
+    // 设置指示条的宽度和位置
+    const selectedItem = ref<HTMLDivElement>(null)
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    onMounted(() => {
+      watchEffect(() => {
+        const { width, left: left1 } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const { left: left2 } = container.value.getBoundingClientRect()
+        const left = left1 - left2
+        indicator.value.style.left = left + 'px'  
+      })      
+    })
+    
     return {
       defaults,
       titles,
       current,
-      select
+      select,
+      indicator,
+      container,
+      selectedItem
     }
   }
 }
@@ -56,6 +75,7 @@ $color: #333;
 $border-color: #d9d9d9;
 .jen-tabs {
   &-nav {
+    position: relative;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
@@ -69,6 +89,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {

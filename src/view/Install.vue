1fe8d558410981b087a35f717586fc96d6ef0b9e
xzw <1272117264@qq.com>
2021-05-14 13:22:58 +0800

    feat: 引入GitHub markdown样式

--- src/view/Install.vue
+++ src/view/Install.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    install
-  </div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install jen-ui</code></pre>
+    <p>或者</p>
+    <pre><code>yarn add jen-ui</code></pre>
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
 </template>

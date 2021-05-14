b666db7d48aa7d2d2e75d364bf930f3020ba7a72
xzw <1272117264@qq.com>
2021-05-14 16:04:30 +0800

    perf: 使用compoent代替组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo />
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo />
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -37,14 +37,11 @@ import Switch2Demo from '../components/Switch2.demo.vue'
 console.log(Switch1Demo)
 export default {
   components: {
-    Switch1Demo,
-    Switch2Demo,
     Switch,
     Button,
   },
   setup() {
     const bool = ref(false)
-
     return { bool, Switch1Demo, Switch2Demo }
   }
 }

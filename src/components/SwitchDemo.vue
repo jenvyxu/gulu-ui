45e60173e29f96f876f9cc9bbb60cd6d9bf6474d
xzw <1272117264@qq.com>
2021-05-14 16:02:12 +0800

    feat: 展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre>{{Switch1Demo.__sourceCode}}</pre>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre>{{Switch2Demo.__sourceCode}}</pre>
       </div>
     </div>
   </div>
@@ -34,7 +34,7 @@ import { ref } from 'vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from '../components/Switch1.demo.vue'
 import Switch2Demo from '../components/Switch2.demo.vue'
-
+console.log(Switch1Demo)
 export default {
   components: {
     Switch1Demo,
@@ -44,7 +44,8 @@ export default {
   },
   setup() {
     const bool = ref(false)
-    return { bool }
+
+    return { bool, Switch1Demo, Switch2Demo }
   }
 }
 </script>

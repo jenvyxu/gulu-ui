6217a784ef73752dac5ddae858315fec851e00c7
xzw <1272117264@qq.com>
2021-05-15 00:44:48 +0800

    perf: 改造ButtonDemo

---
+++ src/components/Button2.demo.vue
@@ -0,0 +1,29 @@
+<demo>支持 size</demo>
+<template>
+  <div>
+    <div>
+      <Button size="big">大按钮</Button>
+      <Button>普通按钮</Button>
+      <Button size="small">小按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大按钮</Button>
+      <Button theme="link">普通按钮</Button>
+      <Button size="small" theme="link">小按钮</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大按钮</Button>
+      <Button theme="text">普通按钮</Button>
+      <Button size="small" theme="text">小按钮</Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button
+  }
+}
+</script>
--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,62 +1,31 @@
 <template>
-  <h1>示例1</h1>
-  <div>
-    <Button>你好</Button>
-    <Button theme="button">你好</Button>
-    <Button theme="link">你好</Button>
-    <Button theme="text">你好</Button>
-    <Button theme="button">normal button</Button>
-    <Button theme="link">link button</Button>
-    <Button theme="text">text button</Button>
-  </div>
-  <h1>示例2</h1>
-  <div>
-    <Button size="small">小按钮</Button>
-    <Button>普通按钮</Button>
-    <Button size="big">大按钮</Button>
-  </div>
-  <h1>示例3</h1>
-  <div>
-    <div>
-      <Button level="main">主要按钮</Button>
-      <Button>普通按钮</Button>
-      <Button level="danger">危险按钮</Button>
-    </div>
-    <div>
-      <Button theme="link" level="main">主要链接按钮</Button>
-      <Button theme="link">普通链接按钮</Button>
-      <Button theme="link" level="danger">危险链接按钮</Button>
-    </div>
-    <div>
-      <Button theme="text" level="main">主要文字按钮</Button>
-      <Button theme="text">普通文字按钮</Button>
-      <Button theme="text" level="danger">危险文字按钮</Button>
-    </div>
-  </div>
-  <h1>示例4</h1>
-  <div>
-    <Button disabled>普通按钮</Button>
-    <Button theme="link" disabled>链接按钮</Button>
-    <Button theme="text" disabled>文字按钮</Button>
-  </div>
-  <h1>示例5</h1>
-  <div>
-    <Button loading>加载中</Button>
-  </div>
+  <h1>Button 示例</h1>
+  <Demo :component="Button1Demo" />
+  <Demo :component="Button2Demo" />
+  <Demo :component="Button3Demo" />
+  <Demo :component="Button4Demo" />
+  <Demo :component="Button5Demo" />
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Demo from '../components/Demo.vue'
+import Button1Demo from '../components/Button1.demo.vue'
+import Button2Demo from '../components/Button2.demo.vue'
+import Button3Demo from '../components/Button3.demo.vue'
+import Button4Demo from '../components/Button4.demo.vue'
+import Button5Demo from '../components/Button5.demo.vue'
 export default {
   components: {
-    Button
+    Demo
   },
   setup(props){
-
+    return {
+      Button1Demo,
+      Button2Demo,
+      Button3Demo,
+      Button4Demo,
+      Button5Demo
+    }
   }
 }
 </script>
-
-<style>
-
-</style>

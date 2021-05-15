422b26dc98999373f3b9c728bb3c991595a475a8
xzw <1272117264@qq.com>
2021-05-15 11:31:57 +0800

    feat: 添加Dialog样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <h1>Dialog 示例</h1>
-    <Dialog />
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="x"/>
   </div>
 </template>
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 export default {
   components: {
-    Dialog
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return {
+      toggle,
+      x
+    }
   }
+
 }
 </script>
 
